3f030ee786446a1a160cf14b3ac977c7d5bbfd3b
kcz <1150791676@qq.com>
2020-03-22 20:29:00 +0800

    修复文件上传组件在下载文件时，文件名无法修改为正确文件名

--- packages/UploadFile/uploadFile.vue
+++ packages/UploadFile/uploadFile.vue
@@ -118,10 +118,51 @@ export default {
     },
     handlePreview(file) {
       // 下载文件
-      let a = document.createElement("a");
-      a.href = file.url || file.thumbUrl;
-      a.download = file.name;
-      a.click();
+      this.getBlob(file.url || file.thumbUrl).then(blob => {
+        this.saveAs(blob, file.name);
+      });
+    },
+    /**
+     * 获取 blob
+     * url 目标文件地址
+     */
+    getBlob(url) {
+      return new Promise(resolve => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.open("GET", url, true);
+        xhr.responseType = "blob";
+        xhr.onload = () => {
+          if (xhr.status === 200) {
+            resolve(xhr.response);
+          }
+        };
+
+        xhr.send();
+      });
+    },
+    /**
+     * 保存 blob
+     * filename 想要保存的文件名称
+     */
+    saveAs(blob, filename) {
+      if (window.navigator.msSaveOrOpenBlob) {
+        navigator.msSaveBlob(blob, filename);
+      } else {
+        const link = document.createElement("a");
+        const body = document.querySelector("body");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = filename;
+
+        // fix Firefox
+        link.style.display = "none";
+        body.appendChild(link);
+
+        link.click();
+        body.removeChild(link);
+
+        window.URL.revokeObjectURL(link.href);
+      }
     },
     remove() {
       this.handleSelectChange();
