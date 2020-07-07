6f2ef8a70fce8b3b589b9a8bad999c9b01676945
kcz <1150791676@qq.com>
2020-07-07 23:17:02 +0800

    增加mini包，mini包不包含antd ui组件，用户项目中已使用antd ui组件，可以选择mini包

--- examples/main.js
+++ examples/main.js
@@ -1,57 +1,55 @@
+// 引入@babel/polyfill处理兼容
+import "@babel/polyfill";
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/";
 
-import {
-  KFormDesign,
-  KFormBuild,
-  setFormDesignConfig
-} from "../packages/index";
-import Cmp from "./components/CustomComponent/index.vue";
+import KFormDesign from "../packages/index";
+// import Cmp from "./components/CustomComponent/index.vue";
 // let Cmp = {
 //   name: "cmp",
 //   render: function(h) {
 //     return h("div", "我是自定义组件");
 //   }
 // };
-setFormDesignConfig({
-  title: "测试自定义字段",
-  list: [
-    {
-      type: "demo", // 表单类型
-      label: "自定义组件", // 标题文字
-      icon: "icon-gallery",
-      component: Cmp,
-      options: {
-        defaultValue: undefined,
-        multiple: false,
-        disabled: false,
-        width: "100%",
-        clearable: true,
-        placeholder: "请选择",
-        showSearch: false
-      },
-      model: "",
-      key: "",
-      rules: [
-        {
-          required: false,
-          message: "必填项"
-        }
-      ]
-    }
-  ],
-  uploadFile: "",
-  uploadImage: "",
-  uploadFileName: "",
-  uploadImageName: "",
-  uploadFileData: { data: 1545 },
-  uploadImageData: { data: 1545 },
-  uploadFileHeaders: { data: 1545 },
-  uploadImageHeaders: { data: 1545 }
-});
+// setFormDesignConfig({
+//   title: "测试自定义字段",
+//   list: [
+//     {
+//       type: "demo", // 表单类型
+//       label: "自定义组件", // 标题文字
+//       icon: "icon-gallery",
+//       component: Cmp,
+//       options: {
+//         defaultValue: undefined,
+//         multiple: false,
+//         disabled: false,
+//         width: "100%",
+//         clearable: true,
+//         placeholder: "请选择",
+//         showSearch: false
+//       },
+//       model: "",
+//       key: "",
+//       rules: [
+//         {
+//           required: false,
+//           message: "必填项"
+//         }
+//       ]
+//     }
+//   ],
+//   uploadFile: "",
+//   uploadImage: "",
+//   uploadFileName: "",
+//   uploadImageName: "",
+//   uploadFileData: { data: 1545 },
+//   uploadImageData: { data: 1545 },
+//   uploadFileHeaders: { data: 1545 },
+//   uploadImageHeaders: { data: 1545 }
+// });
 Vue.use(KFormDesign);
-Vue.use(KFormBuild);
 Vue.config.productionTip = false;
 
 new Vue({
