8eb2330fe514a6de5098e02afec0d92162fa14e5
maqzh <361013213@qq.com>
2021-02-27 23:56:53 +0800

    update packages/KFormBuild/buildBlocks.vue.

--- packages/KFormBuild/buildBlocks.vue
+++ packages/KFormBuild/buildBlocks.vue
@@ -14,6 +14,7 @@
       v-for="(tabItem, index) in record.columns"
       :key="index"
       :tab="tabItem.label"
+      :forceRender="true"
     >
       <buildBlocks
         ref="nestedComponents"
