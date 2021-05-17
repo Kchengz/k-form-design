c1a0474d4a402b51003c21b89db31d4b5f4bc2b8
kcz <1150791676@qq.com>
2021-05-17 14:05:36 +0800

    fix: 修复重置表单数据时表单数据缺少标签布局数据的问题

--- packages/KFormDesign/index.vue
+++ packages/KFormDesign/index.vue
@@ -412,6 +412,8 @@ export default {
         config: {
           layout: "horizontal",
           labelCol: { xs: 4, sm: 4, md: 4, lg: 4, xl: 4, xxl: 4 },
+          labelWidth: 100,
+          labelLayout: "flex",
           wrapperCol: { xs: 18, sm: 18, md: 18, lg: 18, xl: 18, xxl: 18 },
           hideRequiredMark: false,
           customStyle: ""
