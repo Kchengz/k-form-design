3ada50fd8c81deec489be68f851312e182bf6ce0
Dinert <1293437362@qq.com>
2021-07-26 15:56:47 +0800

    预览弹窗样式修改

--- packages/KFormPreview/index.vue
+++ packages/KFormPreview/index.vue
@@ -8,6 +8,9 @@
     cancelText="关闭"
     style="top:20px;"
     :destroyOnClose="true"
+    :centered="true"
+    :dialogStyle="dialogStyle"
+    :bodyStyle="bodyStyle"
     :width="`${previewWidth}px`"
   >
     <k-form-build :value="jsonData" @submit="handleSubmit" ref="KFormBuild" />
@@ -26,7 +29,21 @@ export default {
     return {
       visible: false,
       previewWidth: 850,
-      jsonData: {}
+      jsonData: {},
+        dialogStyle: {
+        position: "absolute",
+        right: "150px",
+        left: "150px",
+        minWidth: "700px",
+        width: "auto",
+        top: "56px",
+        bottom: "30px",
+        minHeight: "500px"
+      },
+      bodyStyle: {
+        height: "calc(100% - 108px)",
+        overflow: "auto"
+      }
     };
   },
   components: {
@@ -59,3 +76,25 @@ export default {
   }
 };
 </script>
+<style lang="less" scoped>
+.ant-modal-root::v-deep .ant-modal-body::-webkit-scrollbar{
+  width: 6px;
+    height: 6px;
+}
+.ant-modal-root::v-deep .ant-modal-body::-webkit-scrollbar-thumb {
+    border-radius: 5px;
+    -webkit-box-shadow: inset 0 0 5px rgb(0 0 0 / 20%);
+    box-shadow: inset 0 0 5px rgb(0 0 0 / 20%);
+    background: rgba(0, 0, 0, 0.2);
+    scrollbar-arrow-color: red;
+}
+.ant-modal-root::v-deep .ant-modal-body::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 5px rgb(0 0 0 / 20%);
+    box-shadow: inset 0 0 5px rgb(0 0 0 / 20%);
+    border-radius: 0;
+    background: rgba(0, 0, 0, 0.1);
+}
+.ant-modal-root::v-deep .ant-modal-content {
+  height: 100%;
+}
+</style>
