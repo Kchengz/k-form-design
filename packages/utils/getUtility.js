eec4218cc224424f74aeb6fdd5c3fd45045d6340
kcz <1150791676@qq.com>
2022-10-25 23:39:57 +0800

    chore: 修改getUtility文件

--- packages/utils/getUtility.js
+++ packages/utils/getUtility.js
@@ -1,6 +1,5 @@
 // 发布npm 需要切换到这个
-// export { pluginManager } from "k-form-design";
-// export { revoke } from "k-form-design";
+// export { pluginManager, revoke, nodeSchema } from "k-form-design";
 
 // 本地测试
 export { pluginManager } from "./PluginManager";
