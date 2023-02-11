99f8d868dbd6bff7d01fb65f4dc72ab10370278a
kcz <1150791676@qq.com>
2023-02-11 10:49:23 +0800

    chore: 切换测试配置

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,8 +1,8 @@
 // 发布npm 需要切换到这个
-export { pluginManager, revoke, nodeSchema, lazyLoadTick } from "k-form-design";
+// export { pluginManager, revoke, nodeSchema, lazyLoadTick } from "k-form-design";
 
 // 本地测试
-// export { pluginManager } from "./PluginManager";
-// export { revoke } from "./Revoke";
-// export { nodeSchema } from "./NodeSchema";
-// export { lazyLoadTick } from "./LazyLoadTick";
+export { pluginManager } from "./PluginManager";
+export { revoke } from "./Revoke";
+export { nodeSchema } from "./NodeSchema";
+export { lazyLoadTick } from "./LazyLoadTick";
